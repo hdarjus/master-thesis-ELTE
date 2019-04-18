82bb5be899894b7e57c1654023b272b3859b3f33
Darjus Hosszejni <darjus.hosszejni@wu.ac.at>
2019-04-18 09:18:40 +0200

    changed .ycm_extra_conf.py

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -1,6 +1,6 @@
 def FlagsForFile (filename, **kwargs):
     return {
-            'flags': ['-x', 'c++', '-std=c++14',
+            'flags': ['-x', 'c++', '-std=c++17',
                 '-I', '/home/dhosszejni/.local/include',
                 '-D', '_DEBUG',
                 #'-Wpedantic', '-Wall', '-Wextra', '-Wconversion',
